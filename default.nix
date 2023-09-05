3bda5baa339ea84647392ad7a52be456b9b08022
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-09-05 17:34:18 +0200

    Fix Nix build
    Bump `haskell.nix` and `CHaP`

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , system ? builtins.currentSystem
 , haskellNix ? import
     (builtins.fetchTarball
-      "https://github.com/input-output-hk/haskell.nix/archive/0.0.117.tar.gz")
+      "https://github.com/input-output-hk/haskell.nix/archive/1b4bccb032d5a32fee0f5b7872660c017a0748d2.tar.gz")
     { }
 , iohkNix ? import
     (builtins.fetchTarball
@@ -10,7 +10,7 @@
     { }
 , cardanoPkgs ?
     (builtins.fetchTarball
-    "https://github.com/input-output-hk/cardano-haskell-packages/archive/316e0a626fed1a928e659c7fc2577c7773770f7f.tar.gz")
+    "https://github.com/input-output-hk/cardano-haskell-packages/archive/4278da8003518bcd3707c079639a55b58b772947.tar.gz")
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-unstable
 , nixpkgsArgs ? haskellNix.nixpkgsArgs
 }:
@@ -26,6 +26,10 @@ musl64.haskell-nix.project {
   compiler-nix-name = compiler;
   projectFileName = "cabal.project";
   inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = cardanoPkgs; };
+  sha256map = {
+    "https://github.com/CardanoSolutions/direct-sqlite.git"."82c5ab46715ecd51901256144f1411b480e2cb8b" = "fuKhPnIVsmdbQ2gPBTzp9nI/3/BTsnvNIDa1Ypw1L+Q=";
+    "https://github.com/CardanoSolutions/text-ansi.git"."dd81fe6b30e78e95589b29fd1b7be1c18bd6e700" = "mCFkVltVeOpDfEkQwClEXFAiOV8lSejmrFBRQhmeLDE=";
+  };
   src = musl64.haskell-nix.haskellLib.cleanSourceWith {
     name = "kupo-src";
     src = ./.;
